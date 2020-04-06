c66017ef559c2d05fb55011dfcfd91bbb03ccb89
Arjun Krishna <arjun12345krishna@gmail.com>
2020-04-06 10:54:02 +0530

    .env files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TOKEN = process.env.TELEGRAM_TOKEN || '991436394:AAHxBLFxCl7aAkh91bhDNCJIKrVdRAhdmzI';
+const TOKEN = process.env.TELEGRAM_TOKEN
 const TelegramBot = require('node-telegram-bot-api');
 const options = {
   webHook: {
@@ -12,33 +12,5 @@ const bot = new TelegramBot(TOKEN, options);
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 bot.on('message', function onMessage(msg) {
-//   bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
-    if(msg.text == "/news") {
-        var chatId = msg.chat.id;
-        request(`http://newsapi.org/v2/top-headlines?q=corona&apiKey=4a0bb231b1db4357b1278797ebc07943&country=in`,function(error,response,body) {
-    if(!error && response.statusCode == 200) {
-        bot.sendMessage(chatId, `_Fetching today's headlines on Corona..._`, {parse_mode: 'Markdown'})
-        .then(function(msg) {
-        var res = JSON.parse(body);
-
-        for (var result = 1; result <= res.totalResults; result++) {
-          
-            // var d = new Date(res.articles[result-1].publishedAt);
-            // var presentTime = d.toLocaleTimeString();
-            // var presentDate = d.toLocaleDateString();
-
-            bot.sendMessage(chatId,
-                'Source :  ' + res.articles[result-1].source.name + '\n' +
-                'Author:  '  + res.articles[result-1].author + '\n' +
-                'Published At :  ' + presentDate + ', ' + presentTime + '\n\n' +
-                res.articles[result-1].title + '\n\n' +
-                res.articles[result-1].description + '\n\n' +
-                res.articles[result-1].content + '\n\n' +
-                'Link :\n' + res.articles[result-1].url
-              )
-            }
-          })
-        }
-      })
-    }
+  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
 });
