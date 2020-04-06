992faf45418186c93b968b3926d8060cadca2b15
Arjun Krishna <arjun12345krishna@gmail.com>
2020-04-06 12:09:36 +0530

    003

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ const getData = async function() {
         url: 'http://newsapi.org/v2/top-headlines?q=corona&apiKey=4a0bb231b1db4357b1278797ebc07943&country=in&pageSize=1',
         json: true
     })
-    console.log(json)
+    // console.log(json)
+    return json.map(res => ({
+        source : res.articles[0].source.name,
+        author : res.articles[0].author
+    }))
 }
 (async function() {
     try {
-        await getData();
+        const people =  await getData();
+        console.log(people);
     } catch(e) {
         console.log('Error: ', e);
     }
