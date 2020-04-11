4f768f8ba408ca333acc339e6cf2c23eea5381cf
Arjun Krishna <arjun12345krishna@gmail.com>
2020-04-11 06:09:51 +0530

    001

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('YOUR-API-KEY-HERE');
+const server = express();
+
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+server.use(bodyParser.json());
+
+
+
+
+
 const TOKEN = process.env.TELEGRAM_TOKEN || '991436394:AAHxBLFxCl7aAkh91bhDNCJIKrVdRAhdmzI';
 const TelegramBot = require('node-telegram-bot-api');
 const options = {
@@ -16,23 +31,30 @@ bot.on('message', function onMessage(msg) {
 
 })
 
-bot.onText(/\/news/, (msg, match) => {
-    const news = match.input.split(' ')[1];
-    var chatId = msg.chat.id;
-    if (news === undefined) {
-        var url = `http://newsapi.org/v2/top-headlines?q=corona&apiKey=4a0bb231b1db4357b1278797ebc07943&country=in&pageSize=1`
-        console.log('hai...')
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                bot.sendMessage(chatId, `_Fetching today's headlines on Corona..._`, {
-                        parse_mode: 'Markdown'
-                    })
-                    .then(function (msg) {
-                        var res = JSON.parse(body);
-                        console.log(res)
-                    })
-            }
-        })
 
-    }
+
+
+bot.onText(/\/news/, (msg, match) => {
+    const query = msg.query.query;
+    newsapi.v2.topHeadlines({
+        q: query || 'corona',
+        sources: 'abc-news, al-jazeera-english, bbc-news, bbc-sport, bloomberg, business-insider, business-insider-uk, buzzfeed, cbs-news, cnbc, cnn, crypto-coins-news, daily-mail, el-mundo, engadget, entertainment-weekly, espn, espn-cric-info, financial-times, fortune, fox-news, fox-sports, hacker-news, independent, info-money, liberation, mashable, mirror, mtv-news, mtv-news-uk, national-geographic, nbc-news, news24, newsweek, new-york-magazine, reuters, techcrunch, techradar, the-economist, the-globe-and-mail, the-guardian-au, the-guardian-uk, the-hindu, the-huffington-post, the-lad-bible, the-new-york-times, the-next-web, the-telegraph, the-times-of-india, the-verge, the-wall-street-journal, the-washington-post, time, usa-today, wired',
+    }).then(response => {
+        let responseToSend;
+        if (response.status === 'ok' && response.articles.length > 0) {
+            const articles = response.articles.map(article => {
+                 bot.sendMessage(chatId,{
+                    "title": article.title,
+                    "image_url": article.urlToImage,
+                    "subtitle": article.description,
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": article.url,
+                        "title": "Read Full Article"
+                    }]
+                })
+            });
+
+        }
+    })
 })
