3c12c2eaecae03bb40c84429fbe5fd9f28c33ba3
Arjun Krishna <arjun12345krishna@gmail.com>
2020-04-11 05:32:55 +0530

    001

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ const bot = new TelegramBot(TOKEN, options);
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 bot.on('message', function onMessage(msg) {
-      bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
+    bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
 
+})
+
+bot.onText(/\/news/, (msg, match) => {
+    const news = match.input.split(' ')[1];
+    var chatId = msg.chat.id;
+    if (news === undefined) {
+        var url = `http://newsapi.org/v2/top-headlines?q=corona&apiKey=4a0bb231b1db4357b1278797ebc07943&country=in&pageSize=1`
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                bot.sendMessage(chatId, `_Fetching today's headlines on Corona..._`, {
+                        parse_mode: 'Markdown'
+                    })
+                    .then(function (msg) {
+                        var res = JSON.parse(body);
+                        console.log(res)
+                    })
+            }
+        })
+
+    }
 })
